52fe840b68d4be1d967c4a0da7e27c490e43b8af
Dave <david@vanheeswijck.com>
2025-02-09 23:15:20 +0100

    refactor: move VPC and S3 discovery to dedicated resource classes
    - Move VPC discovery code from aws_service to VPCResource class
    - Move S3 discovery code from aws_service to S3Resource class
    - Fix region handling in VPCResource to use base class region
    - Clean up redundant fields in EC2 instance details
    - Add proper region initialization when creating resource instances

--- app/services/resources/__init__.py
+++ app/services/resources/__init__.py
@@ -1,4 +1,5 @@
 from .base import AWSResourceBase
 from .s3 import S3Resource
+from .vpc import VPCResource
 
-__all__ = ['AWSResourceBase', 'S3Resource']
+__all__ = ['AWSResourceBase', 'S3Resource', 'VPCResource']
